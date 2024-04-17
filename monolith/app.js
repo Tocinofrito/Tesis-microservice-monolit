415fe8fc8346deca1cc6eddc513881502bb21ff2
Tocinofrito <chichamalo@gmail.com>
2024-04-16 19:47:32 -0600

    a

--- monolith/app.js
+++ monolith/app.js
@@ -7,7 +7,9 @@ const port = 3000;
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:8084'
+}));
 app.use('/img', express.static(path.join(__dirname, 'img')));
 const powers = [
   { id: 1, name: 'flying' },
