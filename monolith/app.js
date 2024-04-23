626785295fa82e1eeff84f8cbd15a09861f32926
Tocinofrito <chichamalo@gmail.com>
2024-04-22 21:02:00 -0600

    arreglao

--- monolith/app.js
+++ monolith/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:8084'
+  origin: '*'
 }));
 app.use('/img', express.static(path.join(__dirname, 'img')));
 const powers = [
